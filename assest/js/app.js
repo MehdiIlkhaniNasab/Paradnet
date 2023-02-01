b15560aa2d293e4ac9a56c9ac65a8ef7e09ba980
Mehdi Ilkhani Nasab <mehdiilkhaninasab7@gmail.com>
2023-02-01 22:29:35 +0330

    make comments section

---
+++ assest/js/app.js
@@ -0,0 +1,36 @@
+var studentCommentSwiper = new Swiper('.comments-swiper', {
+
+    loop: true,
+    grabCursor: true,
+    breakpoints: {
+      350: {
+        slidesPerView: 1,
+        spaceBetween: 5,
+      },
+  
+      540: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+  
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 7,
+      },
+      1024: {
+        slidesPerView: 1,
+        spaceBetween: 8,
+      },
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+      clickable: true,
+  
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  
+  });
